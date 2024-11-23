69f13a14ed2eacc3041dd5c40a24bda01d8dd419
Nano Miratus <nanomiratus@gmail.com>
2024-11-23 14:13:46 +0100

    fix file name

--- parse/_exports.js
+++ parse/_exports.js
@@ -1,4 +1,4 @@
 export { default as isVanillaMod } from "./is-vanilla-mod.js";
-export { default as parseLootLists } from "./parse-loot-list.js";
+export { default as parseLootLists } from "./parse-loot-lists.js";
 export { default as parseMod } from "./parse-mod.js";
 export { default as parseVanillaMod } from "./parse-vanilla-mod.js";
